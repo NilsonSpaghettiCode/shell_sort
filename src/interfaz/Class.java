de2ea47efa419db73aa48faa56b1ff8b69672184
Felipe11 <you@example.com>
2021-02-22 00:53:05 -0500

    Se agregó la clase 'class.java' en el repositorio estimado ndea ndea ndea

---
+++ src/interfaz/Class.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class Class 
+{
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(Hola", JOptionPane.INFORMATION_MESSAGE);
+    }
+    
+}
