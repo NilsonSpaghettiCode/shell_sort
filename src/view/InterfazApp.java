f647498457b3b54e5dcceae37de235fe0e9ba71a
NilsonVargasU <nilson.vargas@uniminuto.edu.co>
2021-02-22 17:57:31 -0500

    Interfaz cambiado

--- src/view/InterfazApp.java
+++ src/view/InterfazApp.java
@@ -5,6 +5,8 @@
  */
 package view;
 
+import model.FileReaderDocument;
+
 /**
  *
  * @author USER
@@ -16,6 +18,14 @@ public class InterfazApp {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        InterfazApp ia = new InterfazApp();
+        ia.start();
+    }
+    
+    public void start(){
+    
+    
+    
     }
     
 }
