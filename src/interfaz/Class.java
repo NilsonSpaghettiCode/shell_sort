ed17448411d2b30ad553d0015c3f2a603d3cae9a
NilsonVargasU <nilson.vargas@uniminuto.edu.co>
2021-02-22 16:58:07 -0500

    Se cambiaron nombres de carpetas, se agregaron las clases FileReader.java y controller.java

--- src/interfaz/Class.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.JOptionPane;
-
-public class Class 
-{
-    public static void main(String[] args) {
-        JOptionPane.showMessageDialog(Hola", JOptionPane.INFORMATION_MESSAGE);
-    }
-    
-}
