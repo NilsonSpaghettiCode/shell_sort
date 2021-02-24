6eb353a55b54ac460cba16b3ddbd12f4dd7c3587
Alucarddos <oscar.leiva@uniminuto.edu.co>
2021-02-23 22:52:47 -0500

    Se agregaron las clases, se crea un public string  se agrega el try para excepciones

--- src/model/WriteNewDocument.java
+++ src/model/WriteNewDocument.java
@@ -6,10 +6,35 @@
 
 package model;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.swing.JFileChooser;
+
 /**
  * 
  * @author Sammy Guergachi <sguergachi at gmail.com>
  */
 public class WriteNewDocument {
+    JFileChooser select = new  JFileChooser();
+    File archivo;
+    FileOutputStream salida;
+    
+    
+    public String Guardar(File archivo, String documento) {
+        String mensaje=null;
+        try {
+                    salida=new FileOutputStream(archivo);
+                    byte[] bytxt=documento.getBytes();
+                    salida.write(bytxt);
+                    mensaje="Archivo Guardar";
+            
+        } catch (Exception e) {
+        }
+            return mensaje;
 
+}
+          
 }
